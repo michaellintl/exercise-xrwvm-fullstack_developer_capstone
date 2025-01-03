c59c79f30f90d29f8cac9e1927247ef0e8502c86
Michael Lintl <michael.lintl@example.org>
2025-01-03 09:09:27 -0500

    Added AboutUs and ContactDetails for module 1 submission

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,11 +1,13 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from django.views.generic import TemplateView
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
+    path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
     # # path for registration
 
     # path for login
